7735afba311cb9dafa17dc5590ec301cea4201cf
tarikkotan <68917249+Smiyagon@users.noreply.github.com>
2020-09-20 19:16:38 +0300

    Delete kararYapilari3.py

--- kararYapilari3.py
+++
@@ -1,9 +0,0 @@
-
-try:
-    sayi1 = int(input("Sayı giriniz: "))
-    if sayi1%2 == 0:
-        print("Sayı çifttir")
-    else:
-        print("sayı tektir")
-except:
-    print("Sayısal değer giriniz")
