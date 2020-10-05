8a361e9683a66ebae29107724b70c6bb26b9282b
tarikkotan <68917249+Smiyagon@users.noreply.github.com>
2020-10-05 23:10:16 +0300

    Add files via upload

--- Ödevler/function.py
+++ Ödevler/function.py
@@ -1,4 +1,18 @@
-# kullanıcı dışarıdan sayısal olarak bir dizi gönderecek, siz bunu sayısal diziye çeviren bir metot yazınız.
+# kullanıcı dışarıdan sayısal olarak bir dizi gönderecek, siz bunu sayısal diziye çeviren bir metot yazınız.
 
 
 #  3 4 5 6 7 8 9 0 d aa sdd =>[3,4,5,6,7,8,9.9,11,]
+
+
+def donustrucu(dizi):
+    liste = dizi.split()
+    for harf in range(len(liste)):
+        if liste[harf].count("."):
+            liste[harf] = float(liste[harf])
+        else:
+            liste[harf] = str(liste[harf])
+        
+    return liste
+
+sonuc = donustrucu("3 5 1  21  21.4 asd a")
+print(sonuc)
--- Ödevler/void.py
+++ Ödevler/void.py
@@ -5,3 +5,27 @@
 # 1 x 1 = 1
 #----------
 # 2 x 2 = 4
+
+
+def kare(sayi):
+    for i in range(sayi):
+        for a in range(sayi):
+            if (i == 0 or i == sayi - 1) or (a == 0 or a== sayi-1) :
+                print("*", end = " ")
+            else:
+                print(" " , end= " ")
+        print()
+
+sayi = int(input("değer giriniz : "))
+
+kare(sayi)
+
+
+def carpimTablosu(sayi):
+    print("Çarpım Tablosu", sayi)
+    for i in range(0, 11):
+        print("------------------")
+        print(sayi,"X",i,"=",sayi * i)
+
+sayi = int(input("Sayı Giriniz: "))
+carpimTablosu(sayi)
